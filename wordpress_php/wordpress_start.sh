25aed8f4cd3e67c9fce7b75c653c94873eee4b0a
pdlmn <jseldardw@gmail.com>
2024-12-01 14:23:39 +0100

    feat: made the whole thing work

--- wordpress_php/wordpress_start.sh
+++ wordpress_php/wordpress_start.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
-cd /var/www/html/
-
-#if [ ! -f wp-config.php ]; then
-    rm -rf ./*
-    wp core download --version="6.3.2" --allow-root
+if [ ! -f /var/www/html/wp-config.php ]; then
+    cd /var/www/html/
+    wp core download --allow-root
     wp config create --dbname=wordpress --dbuser=$SQL_USER --dbpass=$SQL_PASSWORD --dbhost=mariadb --allow-root
-    wp core install --url=localhost --title=inception --admin_user=$WP_ADMIN_USER --admin_password=$WP_ADMIN_PASSWORD --admin_email=admin@example.com  --allow-root
-#fi
+    wp core install --url=localhost --title=inception --admin_user=$WP_ADMIN_USER --admin_password=$WP_ADMIN_PASSWORD --admin_email=$WP_ADMIN_EMAIL  --allow-root
+    wp user create "$WP_REGULAR_USER" "$WP_REGULAR_EMAIL" --user_pass="$WP_REGULAR_PASSWORD" --allow-root
+fi
 exec "$@"
