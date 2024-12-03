b5ae1e2b2310a249940b8e19dc1cfc5cef36c60f
pdlmn <jseldardw@gmail.com>
2024-12-03 22:54:40 +0100

    fix: fixed the mariadb script

--- srcs/requirements/mariadb/tools/mariadb_start.sh
+++ srcs/requirements/mariadb/tools/mariadb_start.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 mariadb-install-db
 mariadb-upgrade
-sed -i "s/DUMMY_PASSWORD/${SQL_PASSWORD}/" /etc/mysql/init.sql
-sed -i "s/DUMMY_USER/${SQL_USER}/" /etc/mysql/init.sql
+# change the values in the SQL script
+sed -i "s/DUMMY_PASSWORD/${SQL_PASSWORD}/g" /etc/mysql/init.sql
+sed -i "s/DUMMY_USER/${SQL_USER}/g" /etc/mysql/init.sql
 # could use mariadb-safe instead, but use just mariadb for better logs
 exec "$@"
