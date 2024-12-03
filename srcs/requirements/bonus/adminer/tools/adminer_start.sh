41c3fb16d558b33ec7053d23a815359fddbad7f0
pdlmn <jseldardw@gmail.com>
2024-12-03 22:49:33 +0100

    doc: updated comment in the adminer script

--- srcs/requirements/bonus/adminer/tools/adminer_start.sh
+++ srcs/requirements/bonus/adminer/tools/adminer_start.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+# moved at runtime instead of in the image because the file needs to be
+# in the volume, which doesn't exist at the image creation step yet
 mv /adminer-4.8.1-en.php /var/www/html/adminer.php
 
 exec "$@"
