40a636a10a2b48ecffcfe4880c423cb84bbeb381
pdlmn <jseldardw@gmail.com>
2024-12-06 18:06:46 +0100

    feat: added redis container, updated wp to install redis plugin fix: fixed adminer script

--- srcs/requirements/bonus/adminer/tools/adminer_start.sh
+++ srcs/requirements/bonus/adminer/tools/adminer_start.sh
@@ -2,6 +2,7 @@
 # moved at runtime instead of in the image because the file needs to be
 # in the volume, which doesn't exist at the image creation step yet
 if [ ! -f /var/www/html/adminer.php ]; then
+    wget https://github.com/vrana/adminer/releases/download/v4.8.1/adminer-4.8.1-en.php
     mv /adminer-4.8.1-en.php /var/www/html/adminer.php
 fi
 exec "$@"
--- srcs/requirements/wordpress_php/tools/wordpress_start.sh
+++ srcs/requirements/wordpress_php/tools/wordpress_start.sh
@@ -5,5 +5,15 @@ if [ ! -f /var/www/html/wp-config.php ]; then
     wp config create --dbname=wordpress --dbuser=$SQL_USER --dbpass=$SQL_PASSWORD --dbhost=mariadb --allow-root
     wp core install --url=$DOMAIN_NAME --title=inception --admin_user=$WP_ADMIN_USER --admin_password=$WP_ADMIN_PASSWORD --admin_email=$WP_ADMIN_EMAIL  --allow-root
     wp user create "$WP_REGULAR_USER" "$WP_REGULAR_EMAIL" --user_pass="$WP_REGULAR_PASSWORD" --allow-root
+
+    # definitions for the redis
+    wp config set WP_REDIS_HOST redis --allow-root
+    wp config set WP_REDIS_PORT 6379 --allow-root # default port of redis
+    wp config set WP_CACHE_KEY_SALT $DOMAIN_NAME --allow-root
+    wp config set WP_REDIS_CLIENT phpredis --allow-root
+    wp plugin install redis-cache --activate --allow-root
+    wp plugin update --all --allow-root
+    wp redis enable --allow-root
+    # to check if redis is working go to /wp-admin -> plugins
 fi
 exec "$@"
