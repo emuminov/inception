37794699b0e8c0ecc4806c2919137c0a46ed7d57
pdlmn <jseldardw@gmail.com>
2024-12-07 01:18:40 +0100

    fix: added -y param to apt update in all docker files fix: fixed the script of ftp

--- srcs/requirements/bonus/ftp/tools/ftp_start.sh
+++ srcs/requirements/bonus/ftp/tools/ftp_start.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-adduser $FTP_USER --disabled-password --quiet
+adduser $FTP_USER --disabled-password --gecos "" --quiet &> /dev/null
 echo "$FTP_USER:$FTP_PASSWORD" | /usr/sbin/chpasswd &> /dev/null
 
 mkdir /home/$FTP_USER/ftp/files -p
-chown nobody:nogroup /home/ftp_user/ftp
+chown nobody:nogroup /home/$FTP_USER/ftp
 
 chmod a-w /home/$FTP_USER/ftp
 chown -R "$FTP_USER:$FTP_USER" /home/$FTP_USER
