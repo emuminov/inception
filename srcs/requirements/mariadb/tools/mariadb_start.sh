ec47fe8cb0b232e3cebe51db0b7d3dd6b3dde86f
Eldar Muminov <emuminov@z1r6p4.42lyon.fr>
2024-12-09 15:28:08 +0100

    fix: reversed changes

--- srcs/requirements/mariadb/tools/mariadb_start.sh
+++ srcs/requirements/mariadb/tools/mariadb_start.sh
@@ -4,7 +4,5 @@ mariadb-upgrade
 # change the values in the SQL script
 sed -i "s/DUMMY_PASSWORD/${SQL_PASSWORD}/g" /etc/mysql/init.sql
 sed -i "s/DUMMY_USER/${SQL_USER}/g" /etc/mysql/init.sql
-useradd mariadb
-chown -R mariadb /var/lib/mysql/
 # could use mariadb-safe instead, but use just mariadb for better logs
 exec "$@"
