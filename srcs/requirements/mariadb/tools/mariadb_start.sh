0bba468300a1477ffb6184d6ee080127f47c8249
Eldar Muminov <emuminov@z1r6p4.42lyon.fr>
2024-12-09 14:49:47 +0100

    fix: changed ownership of mariadb data to its own user

--- srcs/requirements/mariadb/tools/mariadb_start.sh
+++ srcs/requirements/mariadb/tools/mariadb_start.sh
@@ -4,5 +4,7 @@ mariadb-upgrade
 # change the values in the SQL script
 sed -i "s/DUMMY_PASSWORD/${SQL_PASSWORD}/g" /etc/mysql/init.sql
 sed -i "s/DUMMY_USER/${SQL_USER}/g" /etc/mysql/init.sql
+useradd mariadb
+chown -R mariadb/var/lib/mysql/
 # could use mariadb-safe instead, but use just mariadb for better logs
 exec "$@"
